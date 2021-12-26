109072b1a7695546933e03860de8ff4eb6d3bfac
RupinVijan <rupinvijan@gmail.com>
2021-12-26 20:28:42 +0530

    forth commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const cors = require('cors')
 const app = express()
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 
 
